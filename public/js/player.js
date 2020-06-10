a30b3c864b58bd017bccaa552578abf62a53a89d
Laura Junco <lc.junco10@uniandes.edu.co>
2020-06-10 11:23:52 -0500

    Agregar play/pause

--- public/js/player.js
+++ public/js/player.js
@@ -3,6 +3,7 @@
   var audio = "";
   var $bot = "";
   var $marquee = "";
+  var marquee = "";
 
   /*$( ".parches .parche" ).each(function() {
 
@@ -25,6 +26,9 @@
     $container = $(this).find('.transcripcion');
     $marquee = $( "<marquee class='transcripcion-marquee' scrollamount='15'> <p> Responder, soy lucy vengo de los Estados Unidos y ahora vivo en Canadá, crecí cerca de un lago y siempre me ha encantado el agua, para mi la magia de este proceso viene de la forma en que cada creación genera una respuesta, un regalo que luego retorna a sus remitentes para que le continúen. Cada nueva creación nos conmueve y nuestra respuesta nos conecta, aquí hasta el amor por el agua recordando lo que fue y lo que puede ser.</p></marquee>");
     $container.append($marquee);
+
+    marquee = $marquee[0];
+    marquee.stop();
   });
 
 
@@ -33,14 +37,14 @@
 $('.parches .parche').click(function() {
   audio = $(this).find('audio')[0];
   $bot = $(this);
-  $marquee = $(this).find('marquee')[0];
+  marquee = $(this).find('marquee')[0];
   playPauseAudio();
   return false;
 });
 
 audio.onended = function() {
   console.log ("The audio has ended");
-  $marquee.start();
+  marquee.start();
   audio.currentTime = 0;
   pauseAudio();
 };
@@ -62,7 +66,7 @@ audio.onended = function() {
     $bot.removeClass('playing').removeClass('paused');
     $bot.addClass('playing');
     audio.play();
-    $marquee.start();
+    marquee.start();
   }
 
   //Pausar la pista de audio
@@ -70,7 +74,7 @@ audio.onended = function() {
     $bot.removeClass('playing').removeClass('paused');
     $bot.addClass('paused');
     audio.pause();
-    $marquee.stop();
+    marquee.stop();
   }
 
 
