a748de2dc7193cf269f4a1e019010c4ca1b5d87d
Laura Junco <lc.junco10@uniandes.edu.co>
2020-07-30 15:34:52 -0500

    Empezar linea de tiempo

---
+++ public/js/timeline.js
@@ -0,0 +1,21 @@
+
+
+    /*
+    {
+          speed: 9000,
+          autoplay: true,
+          autoplaySpeed: 0,
+          cssEase: 'linear',
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true,
+    }*/
+     
+
+$(document).ready(function(){
+    $('.linea-tiempo').slick({
+        slidesToShow: 6,
+        pauseOnHover: true,
+        
+    });
+  });
