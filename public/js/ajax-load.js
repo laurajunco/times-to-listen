9f0b5b66e3fc8dfc85d161cba6bad330d9704347
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-10 15:49:11 -0500

    Linea de tiempo

--- public/js/ajax-load.js
+++ public/js/ajax-load.js
@@ -51,7 +51,24 @@ $( "#barra_5" ).load( "ajax/sonson.html", function() {
         $('.slider').slick('slickPlay')
     });
 })
-$( "#barra_6" ).load( "ajax/choiba.html")
+$( "#barra_6" ).load( "ajax/choiba.html", function() {
+    $('.linea-tiempo.choiba').slick({
+        slidesToShow: 6,
+        speed: 3000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        infinite: true
+    });
+
+    $('.slider').mouseover(function() {
+        $('.slider').slick('slickPlay')
+    });
+})
+
+
 $( "#barra_7" ).load( "ajax/hacer.html")
 $( "#barra_8" ).load( "ajax/creditos.html")
 $( "#menu" ).load( "ajax/menu.html")
