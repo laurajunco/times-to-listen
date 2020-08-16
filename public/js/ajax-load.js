899ced3589a26ea2661cda9875c81c408b58fde2
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-16 11:06:47 -0500

    Quitar botones de cargar

--- public/js/ajax-load.js
+++ public/js/ajax-load.js
@@ -1,10 +1,6 @@
-var contador = 0;
-var numBarras = 8;
-
-$( "#barra_2" ).load( "ajax/intro.html", addContador())
+$( "#barra_2" ).load( "ajax/intro.html")
 
 $( "#barra_3" ).load( "ajax/bojaya.html" , function() {
-    addContador()
     $('.linea-tiempo.bojaya').slick({
         slidesToShow: 6,
         speed: 10000,
@@ -18,7 +14,6 @@ $( "#barra_3" ).load( "ajax/bojaya.html" , function() {
 })
 
 $( "#barra_4" ).load( "ajax/mampujan.html", function() {
-    contador++
     $('.linea-tiempo.mampujan').slick({
         slidesToShow: 6,
         speed: 10000,
@@ -32,7 +27,6 @@ $( "#barra_4" ).load( "ajax/mampujan.html", function() {
 })
 
 $( "#barra_5" ).load( "ajax/sonson.html", function() {
-    addContador()
     $('.linea-tiempo.sonson').slick({
         slidesToShow: 6,
         speed: 10000,
@@ -45,7 +39,6 @@ $( "#barra_5" ).load( "ajax/sonson.html", function() {
     }); 
 })
 $( "#barra_6" ).load( "ajax/choiba.html", function() {
-    addContador()
     $('.linea-tiempo.choiba').slick({
         slidesToShow: 6,
         speed: 10000,
@@ -58,16 +51,6 @@ $( "#barra_6" ).load( "ajax/choiba.html", function() {
     });
 })
 
-$( "#barra_7" ).load( "ajax/hacer.html", addContador())
-$( "#barra_8" ).load( "ajax/creditos.html", addContador());
-$( "#menu" ).load( "ajax/menu.html", addContador());
-
-
-//solo se activa el umbral cuando todo haya cargado
-function addContador() {
-    contador++;
-    if(contador === numBarras) {
-        $('.cargando').hide();
-        $('.botones').show();
-    }
-}
+$( "#barra_7" ).load( "ajax/hacer.html")
+$( "#barra_8" ).load( "ajax/creditos.html");
+$( "#menu" ).load( "ajax/menu.html");
