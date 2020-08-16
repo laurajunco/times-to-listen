9e8cf71e9a6d17bcce5318676b3a42fcf854af7c
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-16 12:14:16 -0500

    Empezar a agregar botones de navegacion

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -29,7 +29,7 @@ var lineas = [
     {id: 6,linea:'.linea-tiempo.choiba'}, 
 ];
 
-var id = 3; // Iniciar en la seccion 1
+var id = 2; // Iniciar en la seccion 1
 var $selected;
 
 $menu.hide(); //ocultar el menu
@@ -68,6 +68,18 @@ $menu.on("click", '#ir-bojaya', function() {
     id = 3;
     seleccionarBarra(id);
 });
+
+/* flecha siguiente o anterior */
+$('body').on("click", '.flecha-nav .siguiente', function() {
+    id++
+    seleccionarBarra(id);
+})
+
+$('body').on("click", '.flecha-nav .anterior', function() {
+    id--
+    seleccionarBarra(id--);
+})
+
 
 $menu.on("click", '#ir-mampujan', function() {
     $menu.removeClass('cerrado').removeClass('abierto')
