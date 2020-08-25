a82af5860dbdb7ccc9fbd5405f9cc7b46ff9b848
Laura Junco <lc.junco10@uniandes.edu.co>
2020-08-24 19:48:12 -0500

    Terminar programación para mostrar transcripciones

--- public/js/piezas.js
+++ public/js/piezas.js
@@ -2,10 +2,18 @@
 
     var audio = "";
     var $bot = "";
+    var iden ="";
+    var trans = "";
   
     $('body').on("mouseenter",'.rect', function() {
+
         audio = $(this).find('audio')[0];
         $bot = $(this);
+        iden = $(this).attr("trans");
+        trans = $("#" + iden);
+
+        console.log(trans);
+        trans.show();
         audio.play();
         return false;
     });
@@ -13,6 +21,7 @@
     $('body').on("mouseleave",'.rect', function() {
         audio.currentTime = 0;
         audio.pause();
+        trans.hide();
         return false;
     });
     
